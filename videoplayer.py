1f17065e25410d40a4c2bd7d6ab8c2ac09730ec6
Dorry <dorry457@nate.com>
2016-03-13 03:39:30 +0900

    스크롤박스 추가

--- videoplayer.py
+++ videoplayer.py
@@ -6,12 +6,9 @@ from gi.repository import GdkX11, GstVideo
 
 class VideoPlayer(Gtk.Window):
     def __init__(self, app):
-        super(VideoPlayer, self).__init__(parent=app)
+        super(VideoPlayer, self).__init__(type=Gtk.WindowType.TOPLEVEL)
         
         self.app = app
-        self.set_size_request(640, 480)
-        #self.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(20000, 20000, 20000))
-        #self.set_border_width(6)
         self.set_transient_for(app)
         self.set_destroy_with_parent(True)
         self.set_modal(True)
@@ -24,12 +21,12 @@ class VideoPlayer(Gtk.Window):
         
         self.video_frame = Gtk.DrawingArea()
         self.video_frame.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(0, 0, 0))
+        self.video_frame.set_size_request(640, 480)
         vbox.pack_start(self.video_frame, True, True, 0)
         
         # play control
         ctrl2_hbox = Gtk.HBox()
         ctrl2_hbox.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(20000, 20000, 20000))
-        
         
         btn_img = Gtk.Image()
         btn_img.set_from_stock(Gtk.STOCK_MEDIA_PREVIOUS, Gtk.IconSize.BUTTON)
@@ -73,7 +70,7 @@ class VideoPlayer(Gtk.Window):
         self.next_btn.set_relief(Gtk.ReliefStyle.NONE)
         ctrl2_hbox.pack_start(self.next_btn, False, False, 0)
         
-        vbox.pack_end(ctrl2_hbox, False, False, 0)
+        vbox.pack_end(ctrl2_hbox, False, False, 2)
         
         #play time, progress
         ctrl1_hbox = Gtk.HBox(spacing=4)
@@ -94,7 +91,14 @@ class VideoPlayer(Gtk.Window):
         self.lbl_dur.modify_fg(Gtk.StateType.NORMAL, Gdk.Color(65535, 30000, 0))
         ctrl1_hbox.pack_start(self.lbl_dur, False, False, 0)
         
-        vbox.pack_end(ctrl1_hbox, False, False, 0)
+        vbox.pack_end(ctrl1_hbox, False, False, 3)
+        
+        sc_win = Gtk.ScrolledWindow()
+        sc_win.set_size_request(200, -1)
+        sc_win.set_border_width(4)
+        sc_win.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        
+        hbox.pack_end(sc_win, False, True, 0)
         
         self.show_all()
         
