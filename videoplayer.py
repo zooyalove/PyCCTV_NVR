06b1903317bde2893fd422082301d48230962521
dorry457 <dorry457@nate.com>
2016-03-13 10:12:21 +0900

    컨트롤 버튼 간략화

--- videoplayer.py
+++ videoplayer.py
@@ -5,19 +5,34 @@ from gi.repository import Gst, Gtk, Gdk
 from gi.repository import GdkX11, GstVideo
 
 class VideoPlayer(Gtk.Window):
+    player_title = u'PyCCTV_NVR VideoPlayer'
     def __init__(self, app):
         super(VideoPlayer, self).__init__(type=Gtk.WindowType.TOPLEVEL)
         
         self.app = app
+        
+        self.set_title(self.player_title)
+        self.set_border_width(2)
+        self.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(20000, 20000, 20000))
         self.set_transient_for(app)
         self.set_destroy_with_parent(True)
         self.set_modal(True)
         
+        self.setupUI()
+        
+    def setupUI(self):
         hbox = Gtk.HBox()
         self.add(hbox)
         
         vbox = Gtk.VBox()
         hbox.add(vbox)
+        
+        sc_win = Gtk.ScrolledWindow()
+        sc_win.set_size_request(200, -1)
+        sc_win.set_border_width(4)
+        sc_win.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        
+        hbox.pack_end(sc_win, False, True, 0)
         
         self.video_frame = Gtk.DrawingArea()
         self.video_frame.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(0, 0, 0))
@@ -26,55 +41,35 @@ class VideoPlayer(Gtk.Window):
         
         # play control
         ctrl2_hbox = Gtk.HBox()
-        ctrl2_hbox.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(20000, 20000, 20000))
         
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_PREVIOUS, Gtk.IconSize.BUTTON)
         self.prev_btn = Gtk.Button()
-        self.prev_btn.set_image(btn_img)
-        self.prev_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.prev_btn, False, False, 0)
-        
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_REWIND, Gtk.IconSize.BUTTON)
         self.rewind_btn = Gtk.Button()
-        self.rewind_btn.set_image(btn_img)
-        self.rewind_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.rewind_btn, False, False, 0)
-        
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_PLAY, Gtk.IconSize.BUTTON)
         self.play_btn = Gtk.Button()
-        self.play_btn.set_image(btn_img)
-        self.play_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.play_btn, False, False, 0)
-        
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_STOP, Gtk.IconSize.BUTTON)
         self.stop_btn = Gtk.Button()
-        self.stop_btn.set_image(btn_img)
-        self.stop_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.stop_btn, False, False, 0)
-        
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_FORWARD, Gtk.IconSize.BUTTON)
         self.forward_btn = Gtk.Button()
-        self.forward_btn.set_image(btn_img)
-        self.forward_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.forward_btn, False, False, 0)
-        
-        btn_img = Gtk.Image()
-        btn_img.set_from_stock(Gtk.STOCK_MEDIA_NEXT, Gtk.IconSize.BUTTON)
         self.next_btn = Gtk.Button()
-        self.next_btn.set_image(btn_img)
-        self.next_btn.set_relief(Gtk.ReliefStyle.NONE)
-        ctrl2_hbox.pack_start(self.next_btn, False, False, 0)
+
+        ctrl_buttons = ((self.prev_btn, Gtk.STOCK_MEDIA_PREVIOUS, u'이전 영상', self.on_prev_clicked),
+                        (self.rewind_btn, Gtk.STOCK_MEDIA_REWIND, u'10초전으로', self.on_rewind_clicked),
+                        (self.play_btn, Gtk.STOCK_MEDIA_PLAY, u'재생', self.on_play_clicked),
+                        (self.stop_btn, Gtk.STOCK_MEDIA_STOP, u'정지', self.on_stop_clicked),
+                        (self.forward_btn, Gtk.STOCK_MEDIA_FORWARD, u'10초앞으로', self.on_forward_clicked),
+                        (self.next_btn, Gtk.STOCK_MEDIA_NEXT, u'다음 영상', self.on_next_clicked))
+        
+        for btn, st_img, tooltip, func in ctrl_buttons:
+            btn_img = Gtk.Image()
+            btn_img.set_from_stock(st_img, Gtk.IconSize.BUTTON)
+            btn.set_image(btn_img)
+            btn.set_tooltip_text(tooltip)
+            btn.set_relief(Gtk.ReliefStyle.NONE)
+            btn.connect('clicked', func)
+            ctrl2_hbox.pack_start(btn, False, False, 0)
+            
         
         vbox.pack_end(ctrl2_hbox, False, False, 2)
         
-        #play time, progress
+        # play time, progress
         ctrl1_hbox = Gtk.HBox(spacing=4)
-        ctrl1_hbox.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(20000, 20000, 20000))
         
         self.lbl_pos = Gtk.Label('00:00:00')
         self.lbl_pos.set_margin_left(4)
@@ -93,15 +88,27 @@ class VideoPlayer(Gtk.Window):
         
         vbox.pack_end(ctrl1_hbox, False, False, 3)
         
-        sc_win = Gtk.ScrolledWindow()
-        sc_win.set_size_request(200, -1)
-        sc_win.set_border_width(4)
-        sc_win.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        
-        hbox.pack_end(sc_win, False, True, 0)
-        
         self.show_all()
         
+    def on_prev_clicked(self, widget):
+        pass
+        
+    def on_rewind_clicked(self, widget):
+        pass
+
+    def on_play_clicked(self, widget):
+        pass
+
+    def on_stop_clicked(self, widget):
+        pass
+
+    def on_forward_clicked(self, widget):
+        pass
+
+    def on_next_clicked(self, widget):
+        pass
+    
+    
 if __name__ == '__main__':
     from gi.repository import GObject
     
