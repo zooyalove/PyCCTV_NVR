97d87bffe736d1329eddc8077bad44e151748b26
Park Kyungbong <dorry457@nate.com>
2016-04-20 18:04:43 +0900

    preference modified

--- preferences.py
+++ preferences.py
@@ -8,6 +8,8 @@ import os
 import gi
 gi.require_version('Gtk', '3.0')
 
+import xml.etree.ElementTree as ET
+
 from gi.repository import Gtk
 
 DEFAULT_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -35,17 +37,55 @@ DEFAULT_PREF = '''<preferences>
     </video>
     <snapshot prefix="sshot_" />
     <save_dir>%s</save_dir>
-    <>
-    <>
 </preferences>''' % DEFAULT_PATH
+
+
+def xml_parse(source):
+    exists = False
+    if os.path.exists(source) and os.path.isfile(source):
+        data = ET.parse(source)
+        exists = True
+    else:
+        data = ET.fromstring(DEFAULT_PREF)
+    return exists, data.getroot()
 
 
 class Preferences(object):
     '''
     classdocs
     '''
-    def __init__(self):
+    def __init__(self, app, parent):
         '''
         Constructor
         '''
+        self._file = os.path.join(self._app.RESOURCE_PATH, 'preferences.xml')
         self._prefdlg = None
+        self._app = app
+        self._parent = parent
+        self._data = None
+        self._exists = False
+
+        self._data_init()
+
+    def _data_init(self):
+        self._exists, self._data = xml_parse(self._file)
+
+    def _get_preferences(self):
+        pass
+
+    def is_exists(self):
+        return self._exists
+
+    '''
+    @return: preferences's data xml
+    @rtype: Gtk.ResponseType
+    '''
+    def run(self):
+        self._prefdlg = Gtk.Dialog()
+
+        response = self._prefdlg.run()
+        if response == Gtk.ResponseType.OK:
+            pass
+        self._prefdlg.destroy()
+
+        return self._get_preferences()
